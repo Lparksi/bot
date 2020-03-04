a7b908819ba89f20cb95e4027435bd4f1d0536aa
Lparksi <l.moluan@gmail.com>
2020-03-04 10:44:52 +0800

    更改权限

--- awesome/plugins/yiyan/__init__.py
+++ awesome/plugins/yiyan/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import on_command, CommandSession
 from .get import get_yiyan
 
-@on_command('yiyan',aliases=("一言","每日一言"))
+@on_command('yiyan',aliases=("一言","每日一言"),only_to_me=False)
 async def yiyan(session: CommandSession):
     message = get_yiyan()
     await session.send(message)
