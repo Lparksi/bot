b3c8eb6e4a2bed5cac19e555a1ca8bae534e63e7
Lparksi <l.moluan@gmail.com>
2020-03-04 10:39:07 +0800

    更改配置：取消命令启始符

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from nonebot.default_config import *
 from datetime import timedelta
+import re
 
 HOST = '0.0.0.0'
 PORT = 8888
 
 SUPERUSERS = {2726043636}
-COMMAND_START = {'', '/', '!', '／', '！'}
+COMMAND_START = ['', re.compile(r'[/!]+')]
 SESSION_EXPIRE_TIMEOUT = timedelta(minutes=2)
 SESSION_RUN_TIMEOUT = timedelta(seconds=10)
