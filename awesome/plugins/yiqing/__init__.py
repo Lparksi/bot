21d17487dadc8cfc9b60eae882b50e29b52e4fd9
Lparksi <l.moluan@gmail.com>
2020-03-04 21:49:42 +0800

    疫情api失效，重写该插件，安装结巴分词

--- awesome/plugins/yiqing/__init__.py
+++ awesome/plugins/yiqing/__init__.py
@@ -1,28 +1,35 @@
-from nonebot import CommandSession, on_command
+from jieba import posseg
+from nonebot import (CommandSession, IntentCommand, NLPSession, on_command,
+                     on_natural_language)
 
 from .get_yiqing_of_city import get_yiqing_of_city
 
 
-@on_command('yiqing',aliases=('疫情','查疫情'),only_to_me=False)
+@on_command('yiqing',aliases=("疫情","查疫情","疫情详情"))
 async def yiqing(session: CommandSession):
-    city = session.get('city',prompt='你想查询哪个城市的疫情？（仅市级，不带市）')
+    city = session.get('city',prompt="你要查询哪个城市的")
     yiqing_report = get_yiqing_of_city(city)
     await session.send(yiqing_report)
 
 @yiqing.args_parser
 async def _(session: CommandSession):
-    # 去掉消息首尾的空白符
     stripped_arg = session.current_arg_text.strip()
-
     if session.is_first_run:
-        # 该命令第一次运行（第一次进入命令会话）
         if stripped_arg:
-            # 第一次运行参数不为空，意味着用户直接将城市名跟在命令名后面，作为参数传入
-            # 例如用户可能发送了：天气 南京
             session.state['city'] = stripped_arg
-        return
-
+            return
     if not stripped_arg:
-        # 用户没有发送有效的城市名称（而是发送了空白字符），则提示重新输入
-        # 这里 session.pause() 将会发送消息并暂停当前会话（该行后面的代码不会被运行）
-        session.pause('要查询的城市名称不能为空呢，请重新输入')
+       session.pause("要查询的城市不能为空")
+    session.state[session.current_key] = stripped_arg       
+
+@on_natural_language(keywords={"疫情"})
+async def _(session: NLPSession):
+    stripped_msg = session.msg_text.strip()
+    words = posseg.lcut(stripped_msg)
+
+    city = None
+    for word in words:
+        if word.flag == "ns":
+            city = word.word
+            break
+    return IntentCommand(90.0,"yiqing")
