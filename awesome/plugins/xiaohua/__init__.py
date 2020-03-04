221f3723628adb4bfe2444cc55736198703d98f6
Lparksi <l.moluan@gmail.com>
2020-03-04 14:14:47 +0800

    “笑话”插件 only_to_me = False

--- awesome/plugins/xiaohua/__init__.py
+++ awesome/plugins/xiaohua/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import on_command ,CommandSession
 from .get import get_xiaohau
 
-@on_command("xiaohua",aliases=("笑话","讲笑话","讲一个笑话"))
+@on_command("xiaohua",aliases=("笑话","讲笑话","讲一个笑话"),only_to_me=False)
 async def xiaohua(session: CommandSession):
     xh = get_xiaohau()
     await session.send(xh)
