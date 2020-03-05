6e6832e4c0f83d14723d4875b602cdf20a81ad01
Lparksi <l.moluan@gmail.com>
2020-03-05 14:57:18 +0800

    为文言增加timeout限制

--- awesome/plugins/gushiwen_yiyan/get.py
+++ awesome/plugins/gushiwen_yiyan/get.py
@@ -2,9 +2,11 @@ import requests
 
 def get_wenyan():
     url = "https://v1.jinrishici.com/all"
-    r = requests.get(url)
+    r = requests.get(url,timeout=1)
     j = r.json()
     try:
         return f'{j["content"]}\n---{j["author"]}'
+    except Timeout:
+        return "API请求超时！"
     except:
         return "未知错误！"
