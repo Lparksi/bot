546d5b8ef78543d81ccf9160d0cbd40dab7a4668
Lparksi <l.moluan@gmail.com>
2020-03-05 08:27:17 +0800

    将“疫情”插件改回命令，取消对jieba分词的依赖

--- awesome/plugins/yiqing/__init__.py
+++ awesome/plugins/yiqing/__init__.py
@@ -1,11 +1,9 @@
-from jieba import posseg
-from nonebot import (CommandSession, IntentCommand, NLPSession, on_command,
-                     on_natural_language)
+from nonebot import CommandSession, session
 
 from .get_yiqing_of_city import get_yiqing_of_city
 
 
-@on_command('yiqing',aliases=("疫情","查疫情","疫情详情"))
+@on_command('yiqing',aliases=("疫情","查疫情","疫情详情"),)
 async def yiqing(session: CommandSession):
     city = session.get('city',prompt="你要查询哪个城市的")
     yiqing_report = get_yiqing_of_city(city)
@@ -21,15 +19,3 @@ async def _(session: CommandSession):
     if not stripped_arg:
        session.pause("要查询的城市不能为空")
     session.state[session.current_key] = stripped_arg       
-
-@on_natural_language(keywords={"疫情"})
-async def _(session: NLPSession):
-    stripped_msg = session.msg_text.strip()
-    words = posseg.lcut(stripped_msg)
-
-    city = None
-    for word in words:
-        if word.flag == "ns":
-            city = word.word
-            break
-    return IntentCommand(90.0,"yiqing")
