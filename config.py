dda34fd7b223915281b4a8cca8b0cee6d69890fd
Lparksi <l.moluan@gmail.com>
2020-03-06 12:20:40 +0800

    Change

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ HOST = '0.0.0.0'
 PORT = 8888
 
 SUPERUSERS = {2726043636}
-COMMAND_START = ['', re.compile(r'[/!]+')]
 COMMAND_START = {'', '/', '!', '／', '！','.'}
 SESSION_EXPIRE_TIMEOUT = timedelta(minutes=2)
 SESSION_RUN_TIMEOUT = timedelta(seconds=10)
