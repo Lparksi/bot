14a1a68e0143a9185e99262b642e1ec8fb74b0c9
Lparksi <l.moluan@gmail.com>
2020-03-06 10:46:47 +0800

    Change help

--- awesome/plugins/help/__init__.py
+++ awesome/plugins/help/__init__.py
@@ -2,4 +2,11 @@ from nonebot import on_command, CommandSession
 
 @on_command("help")
 async def help(session: CommandSession):
-    await session.send("[CQ:image,file=https://s2.ax1x.com/2020/03/06/3b1k1e.png]")
+    await session.send("""HELP:
+    目前插件:
+    查询天气 | 天气，天气预报，查天气
+    查询疫情 | 疫情，查疫情，疫情详情
+    笑话 | 笑话，讲笑话，讲一个笑话
+    一言 | 一言，每日一言
+    文言一言 | 文言文，古诗词，古诗文，古诗词文言，古诗文言
+    复读 | echo""")
