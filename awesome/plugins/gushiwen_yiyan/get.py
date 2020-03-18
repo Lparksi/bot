bcd451f3723afe4c51f154a7b40f0cfb4659b109
Lparksi <l.moluan@gmail.com>
2020-03-18 21:49:34 +0800

    gushiwen_yiyan -> aioohttp

--- awesome/plugins/gushiwen_yiyan/get.py
+++ awesome/plugins/gushiwen_yiyan/get.py
@@ -1,12 +1,21 @@
-import requests
 
-def get_wenyan():
-    url = "https://v1.jinrishici.com/all"
-    r = requests.get(url,timeout=1)
-    j = r.json()
-    try:
-        return f'{j["content"]}\n---{j["author"]}'
-    except Timeout:
-        return "API请求超时！"
-    except:
-        return "未知错误！"
+#def get_wenyan():
+#   url = "https://v1.jinrishici.com/all"
+#    r = requests.get(url,timeout=1)
+#    j = r.json()
+#    try:
+#        return f'{j["content"]}\n---{j["author"]}'
+#    except Timeout:
+#        return "API请求超时！"
+#    except:
+#        return "未知错误！"
+
+import aiohttp
+async def get_wenyan():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://v1.jinrishici.com/all") as resp:
+            j = resp.json()
+            try:
+                return await f'{j["content"]}\n---{j["author"]}'
+            except:
+                return "Error"
