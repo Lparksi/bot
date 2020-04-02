cd4d06fede17788fd445c11b0a2a289587010b94
Lparksi <l.moluan@gmail.com>
2020-04-02 15:04:12 +0800

    change

--- awesome/plugins/Send_Time/__init__.py
+++ awesome/plugins/Send_Time/__init__.py
@@ -7,7 +7,7 @@ from aiocqhttp.exceptions import Error as CQhttpError
 @nonebot.scheduler.scheduled_job('cron',hour='*')
 async def _():
     bot = nonebot.get_bot()
-    now = datetime.ctime(pytz.timezone('Asia/Shanghai'))
+    now = datetime.ctime()
     try:
         await bot.send_group_msg(group_id=672076603,message=str(now))
     except CQhttpError:
