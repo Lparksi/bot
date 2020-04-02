a1626fdcd82b0e3365ed9496f9e1b7c9d327c636
Lparksi <l.moluan@gmail.com>
2020-04-02 14:41:42 +0800

    报时插件

---
+++ awesome/plugins/Send_Time/__init__.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+import nonebot
+import pytz
+from aiocqhttp.exceptions import Error as CQhttpError
+
+@nonebot.scheduler.scheduled_job('cron',hour='*')
+async def _():
+    bot = nonebot.get_bot()
+    now = datetime.ctime(pytz.timezone('Asia/Shanghai'))
+    try:
+        await bot.send_group_msg(group_id=672076603,message=str(now))
+    except CQhttpError:
+        pass
