6533baf4f01815edb862d09f08328cf72fcae96c
Lparksi <l.moluan@gmail.com>
2020-04-24 08:19:08 +0800

    独家广告模块

--- awesome/plugins/ADs/__init__.py
+++ awesome/plugins/ADs/__init__.py
@@ -3,7 +3,7 @@ from nonebot import on_command, CommandSession
 
 @on_command('ads', aliases=("友链", "广告"))
 async def ads(session: CommandSession):
-    await session.send("""https://parski.xyz换友链
+    await session.send("""https://parksi.xyz换友链
 已收录的友链：
 https://skina.cn/
 https://www.myxx-writer.club/
