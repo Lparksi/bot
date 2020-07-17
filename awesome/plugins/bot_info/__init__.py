7c973572f4d9305783a7a3f3324340e9781805fe
Parksi Liu <l.moluan@gmail.com>
2020-07-17 18:56:15 +0800

    info模块添加更多信息

--- awesome/plugins/bot_info/__init__.py
+++ awesome/plugins/bot_info/__init__.py
@@ -7,4 +7,9 @@ async def info(session: CommandSession):
     bot_qq = bot_info["user_id"]
     nickname_dict = await session.bot.get_group_member_info(group_id=session.event.group_id, user_id=session.self_id)
     nickname = nickname_dict["card"]
-    await session.send(f"""登入号：{bot_qq}\n昵称：{nickname}""")
+    qun_info = await session.bot.get_group_info(group_id=session.event.group_id)
+    group_id = qun_info["group_id"]
+    group_name = qun_info["group_name"]
+    member_count = qun_info["member_count"]
+    max_member_count = qun_info["max_member_count"]
+    await session.send(f"""登入号：{bot_qq}\n昵称：{nickname}\n群信息：{group_name}({group_id}),{member_count}/{max_member_count}""")
